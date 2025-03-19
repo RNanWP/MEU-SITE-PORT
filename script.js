6a6a7abae770a4146d4a5d93c8ea7ceb68f6c11a
RNanWP <renan92011@hotmail.com>
2025-03-19 07:46:35 -0300

    Adicionando Custom.css para estilizar apenas o bootstrap sem interferencias e adicionando o bootstrap ao HTML

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href =
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
+document.querySelector(".our_comb").appendChild(link);
