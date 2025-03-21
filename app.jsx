f52477caab3ed405bd55f7c9acb47e6806898ee9
RNanWP <renan92011@hotmail.com>
2025-03-21 06:57:06 -0300

    comentando partes do bootstrap, mais tarde eu tento resolver

--- app.jsx
+++ app.jsx
@@ -1,4 +1,5 @@
 // Criando e Adicionando (Area de Lazer)ao códugo e a logica do site
 
 // Objetivos a serem feitos--------------
-// Arrumar seção da Twitch
+// Arrumar seção da Twitch --- OK
+// Arrumar o Logo Grayscale com Shine Effect --- ... video em 4:55
